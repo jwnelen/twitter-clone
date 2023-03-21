3d8ea5cc99c65f685ae5b819fdf8183fb67599de
Jeroen Nelen <jwnelen@gmail.com>
2023-03-21 17:32:45 +0100

    Trending start is made

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,28 @@ const NavBarItem = ({text, isActive}: { text: string, isActive: boolean }) => {
   )
 }
 
+const TrendingItem = ({
+                        topText,
+                        hashtag,
+                        amount_tweets
+                      }: { topText: string, hashtag: string, amount_tweets: number }) => {
+  const tweetText = amount_tweets > 1 ? "tweets" : "tweet"
+  const displayText = `${amount_tweets} ${tweetText}`
+
+  return (
+    <div className={"flex justify-between"}>
+      <div className={"flex flex-col"}>
+        <span className={"text-sm text-gray-400"}>{topText}</span>
+        <span className={"font-bold"}>{hashtag}</span>
+        <span className={"text-sm text-gray-400"}>{displayText}</span>
+      </div>
+      <div>
+        <TreeDots></TreeDots>
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <>
@@ -90,8 +112,19 @@ export default function Home() {
             </div>
           </div>
           {/* -- Trends voor Jou */}
-          <div className="col-span-1">
-            Trends voor jou
+          <div className="col-span-1 px-5 space-y-4">
+            <div>
+              <input placeholder="Zoeken op Twitter" className="w-full rounded-xl py-2 px-4"/>
+            </div>
+            <div className="min-h-screen bg-gray-700 p-3 rounded-2xl space-y-4">
+              <span className={"text-xl font-bold"}>Trends voor jou</span>
+              {Array.from(Array(10)).map((_, index) => {
+                return <TrendingItem key={index}
+                                     topText={'Trending in Duitsland'}
+                                     hashtag={"#test"}
+                                     amount_tweets={index + 1}></TrendingItem>
+              })}
+            </div>
           </div>
         </div>
       </div>
